f7066cd2d79e0053ebfb689826822ab2f471e146
siimonfairhurst <simon.d.fairhurst@gmail.com>
2026-06-01 10:05:14 +0100

    Update producthunt.js

--- api/producthunt.js
+++ api/producthunt.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Cache-Control', 's-maxage=3600')
+  res.setHeader('Cache-Control', 'no-store')
 
   const token = process.env.PRODUCT_HUNT_TOKEN
   if (!token) return res.json({ projects: [], missing: true })
