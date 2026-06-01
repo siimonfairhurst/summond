5abeab868e13efdaa1b554f03b82295e3006c50c
siimonfairhurst <simon.d.fairhurst@gmail.com>
2026-06-01 11:55:28 +0100

    Update feed.js

--- api/feed.js
+++ api/feed.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Cache-Control', 's-maxage=300')
+  res.setHeader('Cache-Control', 'no-store')
 
   const base = `https://${req.headers.host}`
 
